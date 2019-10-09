91d0e383fdc8b89a7d9f71de9ba5faa2441ac010
a594645875 <594645875@qq.com>
2019-10-09 19:36:55 +0800

    1.一行代码实现restful风格接口

--- src/main/java/com/czc/springboot/demo/dao/ArticleDao.java
+++ src/main/java/com/czc/springboot/demo/dao/ArticleDao.java
@@ -2,7 +2,9 @@ package com.czc.springboot.demo.dao;
 
 import com.czc.springboot.demo.model.Article;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "article",path="articles")
 public interface ArticleDao extends MongoRepository<Article,String> {
         //支持关键字查询，和JPA的用法一样
         Article findByAuthor(String author);
