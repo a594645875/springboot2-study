274c02fbf0a12cd4398d001a0732032b2c524943
a594645875 <594645875@qq.com>
2019-10-09 14:49:16 +0800

    1.结合mybatis

---
+++ src/main/java/com/czc/springboot/demo/generator/ArticleDao.java
@@ -0,0 +1,30 @@
+package com.czc.springboot.demo.generator;
+
+import com.czc.springboot.demo.model.Article;
+import com.czc.springboot.demo.model.ArticleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ArticleDao {
+    long countByExample(ArticleExample example);
+
+    int deleteByExample(ArticleExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Article record);
+
+    int insertSelective(Article record);
+
+    List<Article> selectByExample(ArticleExample example);
+
+    Article selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Article record, @Param("example") ArticleExample example);
+
+    int updateByExample(@Param("record") Article record, @Param("example") ArticleExample example);
+
+    int updateByPrimaryKeySelective(Article record);
+
+    int updateByPrimaryKey(Article record);
+}
