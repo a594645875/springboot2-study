043caea1f71657efac9a54b0433ca63ebaa04d7e
a594645875 <594645875@qq.com>
2019-10-10 18:13:25 +0800

    1.使用切面方式实现ModelAndView的异常处理

--- src/main/java/com/czc/springboot/demo/controller/ExceptionController.java
+++ src/main/java/com/czc/springboot/demo/controller/ExceptionController.java
@@ -1,21 +1,26 @@
 package com.czc.springboot.demo.controller;
 
 import com.czc.springboot.demo.exception.AjaxResponse;
+import com.czc.springboot.demo.exception.ModelView;
+import com.czc.springboot.demo.model.ArticleVO;
 import com.czc.springboot.demo.service.ExceptionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author czc
  * @date 2019/10/10 17:14
  */
+//@Controller
 @RestController
 public class ExceptionController {
 
@@ -33,4 +38,12 @@ public class ExceptionController {
         exceptionService.userBizError(num);
         return AjaxResponse.success();
     }
+
+//    @ModelView
+//    @GetMapping("/freemarker")
+//    public String index(Model model) {
+//        //产生自定义异常
+//        exceptionService.userBizError(-1);
+//        return "fremarkertemp.html";
+//    }
 }
