898515c506ecebcc6f39dd6952ef74f5a8ba98a4
a594645875 <594645875@qq.com>
2019-10-12 11:18:10 +0800

    1.拦截器实现统一访问日志

---
+++ src/main/java/com/czc/springboot/demo/config/log/LogWebMvcConfigurer.java
@@ -0,0 +1,21 @@
+package com.czc.springboot.demo.config.log;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 注册日志拦截器
+ * @author czc
+ */
+@Configuration
+public class LogWebMvcConfigurer implements WebMvcConfigurer {
+
+    //设置排除路径，spring boot 2.*，注意排除掉静态资源的路径，不然静态资源无法访问
+    private final String[] excludePath = {"/static"};
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AccessLogInterceptor()).addPathPatterns("/**").excludePathPatterns(excludePath);
+    }
+}
