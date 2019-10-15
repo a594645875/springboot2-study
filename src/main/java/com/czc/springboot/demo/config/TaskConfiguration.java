24e05c0c8418affdefaa3fa612cc31eb64ae71be
a594645875 <594645875@qq.com>
2019-10-15 17:37:54 +0800

    为异步任务规划线程池

---
+++ src/main/java/com/czc/springboot/demo/config/TaskConfiguration.java
@@ -0,0 +1,31 @@
+package com.czc.springboot.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 线程池配置类
+ * 任务线程池对象taskExecutor
+ */
+@Configuration
+public class TaskConfiguration {
+    @Bean("taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(200);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("taskExecutor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //设置 线程池关闭 的时候 等待 所有任务都完成后，再继续 销毁 其他的 Bean,异步任务 的 销毁 就会先于 数据库连接池对象 的销毁。
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        //设置线程池中 任务的等待时间，如果超过这个时间还没有销毁就 强制销毁，以确保应用最后能够被关闭，而不是阻塞住
+        executor.setAwaitTerminationSeconds(60);
+        return executor;
+    }
+}
---
+++ src/test/java/com/czc/springboot/demo/task/AsyncExecutorTaskTest.java
@@ -0,0 +1,25 @@
+package com.czc.springboot.demo.task;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static java.lang.Thread.sleep;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AsyncExecutorTaskTest {
+    @Autowired
+    private AsyncExecutorTask task;
+
+    @Test
+    public void testAsyncExecutorTask() throws Exception {
+        task.doTaskOneCallback();
+        task.doTaskTwoCallback();
+        task.doTaskThreeCallback();
+
+        sleep(30 * 1000L);
+    }
+}
